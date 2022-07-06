0b7ef3e1f3ca4ab15ab92c5a31e94acef903dc5a
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-07 03:46:40 +0900

    자세히보기 버튼 만들기, 완료버튼 수정

--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,16 +1,20 @@
 import React, { useState }from "react";
 
-
-
 function TodoItem (props) {
-    const [status, setStatus] = useState("할 일 완료!!");
+    const [status, setStatus] = useState("해야 할 일");
+    const [show, setShow] = useState("자세히");
     
     return (
         <div className="todo-item">
-            {props.item}
             <button className="finButton" onClick={()=> { 
-                setStatus(status === "할 일 완료!!" ? "해야 할 일" : "할 일 완료!!");
+                setStatus(status === "해야 할 일" ? "할 일 완료!!" : "해야 할 일");
             }}>{status}</button>
+            
+            {props.item}
+
+            <button className="detail" onClick={()=> { 
+                setShow(show === "자세히" ? "설명 닫기" : "자세히");
+            }}>{show}</button>
         </div>
     )
 }
