cafc6ebf1962aa4ac6bd9ddd46d06576be7d15d3
yunkyeong Jang <alzkdpffk4314@gmail.com>
2022-07-07 03:24:27 +0900

    finButton을 눌러서 글자 바꾸기

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,19 @@ import { React, useState } from 'react';
 import TodoBoard from './component/TodoBoard';
 
 function App() {
-  const [inputValue, setInputValue] = useState('')
-  const [todoList, setTodoList] = useState([])
+  const [inputValue, setInputValue] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
   const addItem = () => {
-    console.log("나는 여기있다아아아아아아", inputValue)
     setTodoList([...todoList, inputValue])
   }
 
   return (
     <main>
-      <input value = {inputValue}type="text" onChange=
+      <input className='title' value = {inputValue} type="text" onChange=
       {(event) => setInputValue(event.target.value)}/>
+      <br/>
+      <textarea className='content'></textarea>
       <button className='addButton' onClick={addItem}>추가</button>
 
       <TodoBoard todoList={todoList}/>
--- src/component/TodoItem.jsx
+++ src/component/TodoItem.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState }from "react";
+
+
 
 function TodoItem (props) {
-
+    const [status, setStatus] = useState("할 일 완료!!");
+    
     return (
         <div className="todo-item">
             {props.item}
-            <button className="finButton">할 일 완료!</button>
+            <button className="finButton" onClick={()=> { 
+                setStatus(status === "할 일 완료!!" ? "해야 할 일" : "할 일 완료!!");
+            }}>{status}</button>
         </div>
     )
 }
